0bb81f86c1cad0abb65da5875d28fc0ac963fbfa
wario <rostranjj@gmail.com>
2020-11-05 15:23:46 -0600

    fix typo `is` for `in`

--- backend/agency/__init__.py
+++ backend/agency/__init__.py
@@ -23,7 +23,7 @@ def create_app(test_config=None):
         DEBUG=True
     )
 
-    if test_config in None:
+    if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
