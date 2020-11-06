27c08566618c5615981f6e2d7b6a688d2d643464
wario <rostranjj@gmail.com>
2020-11-06 15:09:23 -0600

    successfully hook a test db to unit tests

--- backend/test_agency.py
+++ backend/test_agency.py
@@ -2,7 +2,7 @@ import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
 from agency import create_app
-from agency.models import Actor, Movie
+from agency.models import db, Actor
 
 
 class AgencyTestCase(unittest.TestCase):
@@ -11,23 +11,26 @@ class AgencyTestCase(unittest.TestCase):
         self.app = create_app(test_config=True)
         self.client = self.app.test_client
 
+        ctx = self.app.app_context()
+        ctx.push()
+
+        self.db = db
+        self.db.init_app(self.app)
         with self.app.app_context():
-            self.db = SQLAlchemy()
-            self.db.app = self.app
-            self.db.init_app(self.app)
             self.db.create_all()
 
-    def tearDown(self):
-        with self.app.app_context():
-            self.db.session.remove()
-            self.db.drop_all()
+    # def tearDown(self):
+    #     with self.app.app_context():
+    #         self.db.session.remove()
+    #         self.db.drop_all()
 
     def test_get_all_actors(self):
-        res = self.client().get('/actors')
+        assert False
+        # res = self.client().get('/actors')
 
-        self.assertEqual(res.status_code, 200)
-        data = json.loads(res.data)
-        self.assertEqual("application/vnd.api+json", data.mimetype)
+        # self.assertEqual(res.status_code, 200)
+        # data = json.loads(res.data)
+        # self.assertEqual("application/vnd.api+json", data.mimetype)
 
 
 if __name__ == "__main__":
