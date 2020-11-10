521cd88381f45141f38eed018a255d40be47cac5
wario <rostranjj@gmail.com>
2020-11-10 16:17:54 -0600

    implement a m-to-m relationship btwn actor and movie models

--- backend/agency/models.py
+++ backend/agency/models.py
@@ -4,15 +4,27 @@ from flask_migrate import Migrate
 db = SQLAlchemy()
 migrate = Migrate()
 
+assigned_movies = db.Table(
+    'assigned_movies',
+    db.Column('actor_id',
+              db.Integer,
+              db.ForeignKey('actor.id'),
+              primary_key=True),
+    db.Column('movie_id',
+              db.Integer,
+              db.ForeignKey('movie.id'),
+              primary_key=True))
+
 
 class Actor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     age = db.Column(db.Integer)
     gender = db.Column(db.String(1))
-    # movies = db.relationship('Movie',
-    #                          secondary=actor_items,
-    #                          backref=db.backref('actors', lazy=True))
+
+    movies = db.relationship('Movie',
+                             secondary=assigned_movies,
+                             backref=db.backref('actors', lazy=True))
 
     def insert(self):
         """Inserts a new model into the database."""
@@ -29,23 +41,23 @@ class Actor(db.Model):
         db.session.commit()
 
 
-# class Movie(Base):
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(200), nullable=False)
-#     release_date = db.Column(db.DateTime, nullable=False)
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    release_date = db.Column(db.DateTime, nullable=False)
 
-#     def insert(self):
-#         """
-#         Inserts a new model into the database.
-#         """
-#         db.session.add(self)
-#         db.session.commit()
+    def insert(self):
+        """
+        Inserts a new model into the database.
+        """
+        db.session.add(self)
+        db.session.commit()
 
-#     def delete(self):
-#         """Deletes a model from the database."""
-#         db.session.delete()
-#         db.session.commit()
+    def delete(self):
+        """Deletes a model from the database."""
+        db.session.delete()
+        db.session.commit()
 
-#     def update(self):
-#         """Updates an existing model in the database."""
-#         db.session.commit()
+    def update(self):
+        """Updates an existing model in the database."""
+        db.session.commit()
