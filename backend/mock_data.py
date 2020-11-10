9ffcffc7b2a18691b42ea8514cf1a0230684b598
wario <rostranjj@gmail.com>
2020-11-10 12:00:07 -0600

    add mock data file for tests

---
+++ backend/mock_data.py
@@ -0,0 +1,62 @@
+mock_actors = [
+    {
+        'name': 'Pete',
+        'age': 33,
+        'gender': 'M',
+    },
+    {
+        'name': 'Billy Bob',
+        'age': 21,
+        'gender': 'M'
+    },
+    {
+        'name': 'Kurenai',
+        'age': 28,
+        'gender': 'F'
+    },
+    {
+        'name': 'Tsunade',
+        'age': 39,
+        'gender': 'F'
+    },
+    {
+        'name': 'Sakura',
+        'age': 26,
+        'gender': 'F'
+    },
+    {
+        'name': 'Nagato',
+        'age': 28,
+        'gender': 'M'
+    },
+    {
+        'name': 'Naruto',
+        'age': 26,
+        'gender': 'M'
+    },
+    {
+        'name': 'Hashirama',
+        'age': 60,
+        'gender': 'M'
+    },
+    {
+        'name': 'Hinata',
+        'age': 27,
+        'gender': 'F'
+    },
+    {
+        'name': 'Boruto',
+        'age': 13,
+        'gender': 'M'
+    },
+    {
+        'name': 'Minato',
+        'age': 32,
+        'gender': 'M'
+    },
+    {
+        'name': 'Kushina',
+        'age': 29,
+        'gender': 'F'
+    }
+]
